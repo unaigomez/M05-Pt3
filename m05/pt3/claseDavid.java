d04db112fc424cb3bccd03693a49e581569484dc
DSori04 <120370203+DSori04@users.noreply.github.com>
2023-05-04 08:21:27 +0200

    Add files via upload

---
+++ m05/pt3/claseDavid.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author david
+ */
+
+package com.mycompany.m05.pt3;
+public class claseDavid{
+    public int resta (int a, int b){
+        return a-b;
+}
+}
